d3025245916cb8ea3315d145063b1831faae8c82
Robert Adamczewski <robert@adamczewski.me>
2020-05-17 19:31:43 +0200

    feat: add global scss

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,62 +1,59 @@
-
 export default {
-  mode: 'universal',
-  /*
+	mode: 'universal',
+	/*
   ** Headers of the page
   */
-  head: {
-    title: process.env.npm_package_name || '',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-  },
-  /*
+	head: {
+		title: process.env.npm_package_name || '',
+		meta: [
+			{ charset: 'utf-8' },
+			{ name: 'viewport', content: 'width=device-width, initial-scale=1' },
+			{ hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+		],
+		link: [
+			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+		]
+	},
+	/*
   ** Customize the progress-bar color
   */
-  loading: { color: '#fff' },
-  /*
+	loading: { color: '#fff' },
+	/*
   ** Global CSS
   */
-  css: [
-  ],
-  /*
+	css: [
+		'./assets/scss/style.scss'
+	],
+	/*
   ** Plugins to load before mounting the App
   */
-  plugins: [
-  ],
-  /*
+	plugins: [],
+	/*
   ** Nuxt.js dev-modules
   */
-  buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
-    '@nuxtjs/eslint-module'
-  ],
-  /*
+	buildModules: [
+		// Doc: https://github.com/nuxt-community/eslint-module
+		'@nuxtjs/eslint-module'
+	],
+	/*
   ** Nuxt.js modules
   */
-  modules: [
-    // Doc: https://axios.nuxtjs.org/usage
-    '@nuxtjs/axios'
-  ],
-  /*
+	modules: [
+		// Doc: https://axios.nuxtjs.org/usage
+		'@nuxtjs/axios'
+	],
+	/*
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
-  axios: {
-  },
-  /*
+	axios: {},
+	/*
   ** Build configuration
   */
-  build: {
-    /*
+	build: {
+		/*
     ** You can extend webpack config here
     */
-    extend (config, ctx) {
-    }
-  }
-}
+		extend(config, ctx) {}
+	}
+};
