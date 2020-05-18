69ae7021155d6e97e0c2eecafa18e6e8f198fba1
Robert Adamczewski <robert@adamczewski.me>
2020-05-18 20:27:09 +0200

    feat: remove boilerplate markup, add new components view

--- pages/index.vue
+++ pages/index.vue
@@ -2,71 +2,34 @@
   <div class="container">
     <div>
       <logo />
-      <h1 class="title">
-        dzika
-      </h1>
-      <h2 class="subtitle">
-        Website for local restaurant.
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <slogan />
+      <social />
+      <link-button />
+      <page-footer />
+      <page-menu />
     </div>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
+import Logo from '~/components/Header/Logo.vue'
+import Slogan from '~/components/Header/Slogan.vue'
+import Social from '~/components/Aside/Social.vue'
+import LinkButton from '~/components/General/LinkButton.vue'
+import PageFooter from '~/components/Footer/PageFooter.vue'
+import PageMenu from '~/components/Navigation/PageMenu.vue'
 
 export default {
   components: {
-    Logo
+    Logo,
+    Slogan,
+    Social,
+    LinkButton,
+    PageFooter,
+    PageMenu
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
 </style>
