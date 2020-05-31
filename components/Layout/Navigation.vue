1fde391bb0621a4551544a23d66a13abace96575
Robert Adamczewski <robert@adamczewski.me>
2020-05-31 20:16:31 +0200

    feat: create the mobile website navigation

--- components/Layout/Navigation.vue
+++ components/Layout/Navigation.vue
@@ -1,33 +1,51 @@
 <template>
   <ul class="menu">
-    SITE MENU!!!!!!!!!
-    <li
-      v-for="(item, index) in navigationArr"
+    <!-- <div
+      v-for="(item, index) in navigation"
       :key="index"
+    > -->
+    <li
+      v-for="(item, index) in navigation"
+      :key="index"
+      :class="checkRoute(item.url)"
       class="menu__item"
     >
-      <!-- <nav-link
+      <nav-link
         :title="item.title"
-        :link="item.link"
+        :url="item.url"
         class="menu__link"
-      /> -->
+      />
     </li>
+    <!-- </div> -->
   </ul>
 </template>
 
 <script>
-// import NavLink from '~/components/Navigation/NavLink.vue'
+import { mapState } from 'vuex'
+import NavLink from '~/components/Home/Navigation/NavLink.vue'
 
 export default {
   components: {
-    // NavLink
+    NavLink
   },
-  props: {
-    navigationArr: {
-      type: Object,
-      default: () => {
-        return {}
-      }
+  data () {
+    return {
+      nav: []
+    }
+  },
+  computed: {
+    ...mapState(['navigation'])
+  },
+  created () {
+    // eslint-disable-next-line no-console
+    // console.log(this.$route.fullPath)
+    // this.$route.fullPath === '/menu'
+    //   ? this.nav = this.navigation.filter(({ url }) => url === '/events')
+    //   : this.nav = this.navigation.filter(({ url }) => url === '/menu')
+  },
+  methods: {
+    checkRoute (item) {
+      if (this.$route.fullPath === item) { return 'hide-me' }
     }
   }
 }
@@ -39,18 +57,29 @@ export default {
   padding: 0;
   display: flex;
   flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  // background: aquamarine;
+  height: 70px;
+  width: 100%;
+  overflow: hidden;
+  // padding: calc(var(--spacing) * 2) 0;
+  position: fixed;
+  bottom: calc(var(--spacing) * 2);
+  z-index: 10;
 }
 
 .menu__item {
   display: block;
   height: 32px;
-  width: 100%;
+  width: 65%;
+
+  // width: 100%;
   // line-height: 32px;
   // background: var(--c-red);
-  margin-bottom: 16px;
+  // margin-bottom: 4px;
   // border: 1px solid $c-red;
   // transition: 0.5s ease;
-  background: aqua;
 }
 
 // .menu__item:hover {
@@ -70,10 +99,21 @@ export default {
 //   }
 // }
 
-// .menu__link {
-//   font-weight: $fw-normal;
-//   line-height: 32px;
-//   padding-left: 20px;
-//   color: $c-white;
-// }
+.hide-me {
+  display: none;
+  background: orchid;
+}
+
+.menu__link {
+  justify-content: center;
+  border: 1px solid $c-red;
+  border-radius: 16px;
+  // height: 32px;
+  //   font-weight: $fw-normal;
+  //   line-height: 32px;
+  //   padding-left: 20px;
+  //   color: $c-white;
+  // box-shadow: 4px 4px 10px 1.5rem rgba(47, 31, 232, 0.9);
+  box-shadow: 0 0 1rem rgba(208, 42, 39, 0.65);
+}
 </style>
