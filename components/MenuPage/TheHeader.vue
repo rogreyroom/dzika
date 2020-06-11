43af4f9375b7c401ec8320f0d1d05ac0b5a99a3c
Robert Adamczewski <robert@adamczewski.me>
2020-06-11 18:10:59 +0200

    feat: update the component styles

--- components/MenuPage/TheHeader.vue
+++ components/MenuPage/TheHeader.vue
@@ -21,17 +21,22 @@ export default {
 .header {
   --header-background: var(--primary);
   --header-title-font-face: var(--ff-primary);
-  --header-title-font-size: var(--title-normal);
+  --header-title-font-size: var(--title-small);
   --header-title-font-weight: var(--fw-normal);
   --header-title-color: var(--white);
   --header-padding: var(--space-sm);
+  --header-margin: var(--space-sm);
 
   display: flex;
   justify-content: center;
   align-items: center;
   background: var(--header-background);
+  max-width: 320px;
   width: 100%;
   padding: var(--header-padding);
+  margin: 0 auto;
+  margin-bottom: var(--header-margin);
+  line-height: 1.1;
 
   &__title {
     font-family: var(--header-title-font-face);
